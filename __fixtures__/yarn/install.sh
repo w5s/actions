1efadb15a8585c15c3de0e1d84ee1e7e5ed02198
Julien Polo <julien.polo@gmail.com>
2026-06-16 19:39:10 +0200

    ✅ Add tests (#115)

---
+++ __fixtures__/yarn/install.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+yarn install --immutable
---
+++ __fixtures__/yarn/test.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+yarn node -e "const isNumber = require('is-number'); if (!isNumber(42) || !isNumber('42') || isNumber('abc')) { process.exit(1); }"
