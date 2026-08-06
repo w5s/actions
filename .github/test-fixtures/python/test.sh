03e2c0078be0325f05b9a340f7c21e2bad8e5d8a
Julien Polo <julien.polo@gmail.com>
2026-08-06 11:33:11 +0200

    🔧 Update expected Python version check to include minor version

--- .github/test-fixtures/python/test.sh
+++ .github/test-fixtures/python/test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-expected_python_minor="$(awk '$1 == "python" { print $2 }' .tool-versions)"
+expected_python_minor="$(awk '$1 == "python" { split($2, parts, "."); print parts[1] "." parts[2] }' .tool-versions)"
 actual_python_minor="$(python -c "import sys; print(f'{sys.version_info.major}.{sys.version_info.minor}')")"
 [[ "$actual_python_minor" == "$expected_python_minor" ]]
